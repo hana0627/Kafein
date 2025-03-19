c1355c39d68fb5aa1a1d081d63b0bde934287c78
hana0627 <hanana9506@naver.com>
2025-03-19 17:39:43 +0900

    #7 Create APIResponse

---
+++ BaseProject/src/main/kotlin/com/hana/baseproject/core/response/APIResponse.kt
@@ -0,0 +1,24 @@
+package com.hana.baseproject.core.response
+
+import org.springframework.http.HttpStatus.*
+
+data class APIResponse<T> (
+    val resultCode: String, // 왠만하면 항상 200으로 고정
+    val result: T,
+) {
+
+    companion object {
+        fun <T> success(result: T) : APIResponse<T> {
+            return APIResponse(OK.name, result)
+        }
+
+//        fun <T> error(result: T): APIResponse<String> {
+//            return if (result is ApplicationException) {
+//                APIResponse((result as ApplicationException).getErrorCode.status.name, result.getMessage)
+//            } else {
+//                APIResponse(INTERNAL_SERVER_ERROR.name, "알 수 없는 예외가 발생했습니다.")
+//            }
+//        }
+
+    }
+}
