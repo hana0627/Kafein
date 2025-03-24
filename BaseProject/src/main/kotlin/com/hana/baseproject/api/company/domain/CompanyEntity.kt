2f6cd29ea46de6acd444029032e69ea712f3a689
hana0627 <hanana9506@naver.com>
2025-03-24 16:45:12 +0900

    #5 add new column(deleted, deleteDate) in CompanyEntity

--- BaseProject/src/main/kotlin/com/hana/baseproject/api/company/domain/CompanyEntity.kt
+++ BaseProject/src/main/kotlin/com/hana/baseproject/api/company/domain/CompanyEntity.kt
@@ -4,28 +4,42 @@ import com.hana.baseproject.api.company.controller.request.CompanyCreate
 import com.hana.baseproject.api.product.domain.ProductEntity
 import com.hana.baseproject.api.user.domain.UserEntity
 import jakarta.persistence.*
+import java.time.LocalDateTime
 
 @Entity
 @Table(name = "company", indexes = arrayOf(Index(name = "idx_company", columnList = "companyCode")))
-class CompanyEntity (
-    @Column(length = 255, nullable = true)
+data class CompanyEntity (
+    @Column(length = 255, nullable = true, unique = true)
     val companyCode: String,
 
     @Column(length = 255, nullable = true)
-    val companyName: String,
+    var companyName: String,
     @OneToMany(mappedBy = "companyEntity")
     val userEntity: List<UserEntity>,
 
     @OneToMany(mappedBy = "companyEntity")
     val productEntity: List<ProductEntity>,
 
+    var deleted: Boolean = false,
+
+    @Column(nullable = true)
+    var deletedDate: LocalDateTime? = null,
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long?,
 
 
-){
-
+    ){
+    constructor(companyCode: String, companyName: String) : this(
+        companyCode = companyCode,
+        companyName = companyName,
+        userEntity = emptyList(),
+        productEntity = emptyList(),
+        deleted = false,
+        deletedDate = null,
+        null
+    )
 
     companion object {
         fun fixture(
@@ -33,6 +47,8 @@ class CompanyEntity (
             companyName: String = "하나다방",
             userEntity: List<UserEntity> = mutableListOf(),
             productEntity: List<ProductEntity> = mutableListOf(),
+            deleted: Boolean = false,
+            deletedDate: LocalDateTime? = null,
             id: Long? = null
         ) : CompanyEntity {
             return CompanyEntity(
@@ -40,8 +56,22 @@ class CompanyEntity (
                 companyName = companyName,
                 userEntity = userEntity,
                 productEntity = productEntity,
+                deleted = deleted,
+                deletedDate = deletedDate,
                 id = id
             )
         }
     }
+
+    fun updateCompanyName(companyName: String): CompanyEntity {
+        this.companyName = companyName
+        return this
+    }
+
+    fun delete(): CompanyEntity {
+        this.deleted = true
+        this.deletedDate = LocalDateTime.now()
+        return this
+    }
+
 }
