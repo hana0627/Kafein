f4cc2383bb791a6744a5f1b4f2fd30993ecc410e
hana0627 <hanana9506@naver.com>
2025-03-26 16:47:54 +0900

    #5 Add Field in categoryCode and upCategoryCode in ProductTypeEntity

--- BaseProject/src/main/kotlin/com/hana/baseproject/api/product/domain/ProductTypeEntity.kt
+++ BaseProject/src/main/kotlin/com/hana/baseproject/api/product/domain/ProductTypeEntity.kt
@@ -8,14 +8,18 @@ import jakarta.persistence.*
 @Table(name = "product_type")
 class ProductTypeEntity (
 
-
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     val productCategory: ProductCategory,
 
     @Column(length = 100 ,nullable = false)
+    val categoryCode: String,
+
+    @Column(length = 100 ,nullable = false)
     val categoryName: String,
+
+    @Column(length = 100 ,nullable = true)
+    val upCategoryCode: String,
 
     @OneToMany(mappedBy = "productTypeEntity")
     val productEntity: List<ProductEntity>,
