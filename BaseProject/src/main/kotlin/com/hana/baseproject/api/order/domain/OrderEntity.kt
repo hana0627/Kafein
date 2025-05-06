68b84b8c3595e52e89af2d1b2247db87f5b45ef0
hana0627 <hanana9506@naver.com>
2025-05-06 17:27:42 +0900

    #5 add new column(unitPrice, deleted, deletedDate)

--- BaseProject/src/main/kotlin/com/hana/baseproject/api/order/domain/OrderEntity.kt
+++ BaseProject/src/main/kotlin/com/hana/baseproject/api/order/domain/OrderEntity.kt
@@ -4,6 +4,7 @@ import com.hana.baseproject.api.order.domain.constant.OrderStatus
 import com.hana.baseproject.api.product.domain.ProductEntity
 import com.hana.baseproject.api.user.domain.UserEntity
 import jakarta.persistence.*
+import java.time.LocalDateTime
 
 @Entity
 @Table(name = "orders", indexes = arrayOf(Index(name = "idx_user_account", columnList = "order_code")))
@@ -11,6 +12,8 @@ class OrderEntity (
 
     @Column(length = 100, updatable = false, nullable = false)
     val orderCode: String,
+
+    val unitPrice: Int,
 
     val totalPrice: Int,
 
@@ -27,6 +30,11 @@ class OrderEntity (
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     val product: ProductEntity,
+
+    var deleted: Boolean = false,
+
+    @Column(nullable = true)
+    var deletedDate: LocalDateTime? = null,
 
     @Id
     var id: Long?
